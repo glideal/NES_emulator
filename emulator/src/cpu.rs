928886cf96f446499b9fb750556813a90bcb14e6
glideal <[korekurainooventovakoni@gmail.com]>
2024-09-08 20:08:01 +0900

    implement memory

--- emulator/src/cpu.rs
+++ emulator/src/cpu.rs
@@ -6,6 +6,7 @@ pub struct CPU{
     Decimal mode flag|Interpret disable|Zero flag|Carry flag
     */
     pub program_counter:u16,
+    memory:[u8;0xFFFF],
 
 }
 
@@ -16,6 +17,7 @@ impl CPU{
             register_x:0,
             status: 0, //0b0000_0000
             program_counter: 0, 
+            memory:[0;0xFFFF],
         }
     } 
 
@@ -55,16 +57,46 @@ impl CPU{
         }
     }
 
+    /*オペランドなどが8バイトなのに対して、アドレスは16バイト */
+    fn mem_read(&self,addr:u16)->u8{
+        self.memory[addr as usize]
+    }
 
-    pub fn interpret(&mut self, program:Vec<u8>){
-        self.program_counter=0;
+    fn mem_read_u16(&mut self, pos:u16)->u16{
+        let lo=self.mem_read(pos) as u16;
+        let hi =self.mem_read(pos+1) as u16;
+        (hi<<8)|(lo as u16)//<<は左シフト演算子
+    }
+
+    fn mem_write(&mut self, addr:u16,data:u8){
+        self.memory[addr as usize]=data;
+    }
+
+    fn mem_wtite_u16(&mut self, pos:u16,data:u16){
+        let hi=(data>>8) as u8;
+        let lo=(data & 0b00000000_11111111) as u8;
+        self.mem_write(pos,lo);
+        self.mem_write(pos+1,hi);
+    }
+    
+    pub fn load_and_run(&mut self,program:Vec<u8>){
+        self.load(program);
+        self.run();
+    }
+
+    pub fn load(&mut self,program:Vec<u8>){
+        self.memory[0x8000..(0x8000+program.len())].copy_from_slice(&program[..]);
+        //memory[0c8000..(ox8000+program.len())]にprogram[..]の内容を格納する。
+        self.program_counter=0x8000;
+    }
+
+    pub fn run(&mut self){
         loop{
-            let opscode =program[self.program_counter as usize];
+            let opscode=self.mem_read(self.program_counter);
             self.program_counter+=1;
-
             match opscode{
                 0xA9=>{//LDA
-                    let param=program[self.program_counter as usize];
+                    let param=self.mem_read(self.program_counter);
                     self.program_counter+=1;
 
                     self.lda(param);
@@ -84,6 +116,11 @@ impl CPU{
                 _=>todo!()
             }
         }
+    }
+
+
+    pub fn interpret(&mut self, program:Vec<u8>){
+        self.load_and_run(program);
     }
 }
 
