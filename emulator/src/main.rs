c584fd6d43e95d8c1d15b9b96acd1d7a0f5f5774
glideal <[korekurainooventovakoni@gmail.com]>
2024-09-08 16:47:16 +0900

    easy to view program

--- emulator/src/main.rs
+++ emulator/src/main.rs
@@ -19,6 +19,31 @@ impl CPU{
         }
     } 
 
+    fn lda(&mut self, value:u8){
+        self.register_a=value;
+        self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    fn tax(&mut self){
+        self.register_x=self.register_a;
+        self.update_zero_and_negative_flags(self.register_x);
+    }
+
+    fn update_zero_and_negative_flags(&mut self, result:u8){
+        if result==0{//Zero flag
+            self.status=self.status|0b0000_0010;
+        }else{
+            self.status=self.status&0b1111_1101;
+        }
+
+        if result&0b1000_0000!=0{//negative flag
+            self.status=self.status|0b1000_0000;
+        }else{
+            self.status=self.status&0b0111_1111;
+        }
+    }
+
+
     pub fn interpret(&mut self, program:Vec<u8>){
         self.program_counter=0;
         loop{
@@ -29,35 +54,14 @@ impl CPU{
                 0xA9=>{//LDA
                     let param=program[self.program_counter as usize];
                     self.program_counter+=1;
-                    self.register_a=param;
 
-                    if self.register_a==0{//Zero flag
-                        self.status=self.status|0b0000_0010;
-                    }else{
-                        self.status=self.status&0b1111_1101;
-                    }
-
-                    if self.register_a&0b1000_0000!=0{//negative flag
-                        self.status=self.status|0b1000_0000;
-                    }else{
-                        self.status=self.status&0b0111_1111;
-                    }
+                    self.lda(param);
                 }
-                0xAA=>{//TAX
-                    self.register_x=self.register_a;
-
-                    if self.register_x==0{//Zero flag
-                        self.status=self.status|0b0000_0010;
-                    }else{
-                        self.status=self.status&0b1111_1101;
-                    }
-
-                    if self.register_x&0b1000_0000!=0{//negative flag
-                        self.status=self.status|0b1000_0000;
-                    }else{
-                        self.status=self.status&0b0111_1111;
-                    }
+                
+                0xAA=>{
+                    self.tax()
                 }
+
                 0x00=>{
                     return;
                 }
@@ -66,6 +70,7 @@ impl CPU{
         }
     }
 }
+
 
 fn main(){
     println!("hello world");
